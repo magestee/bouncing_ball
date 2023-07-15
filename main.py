8f79b344d3fde15f9b62dd9851d68fa27d27c673
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-07-15 20:38:56 +0330

    bounce calculation functionality added

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ xmax = 10
 
 dt = 0.001
 
+x0, y0 = 0, 5
+vx0, vy0 = 1, 0
+
 def get_position(t=0):
-    x, y, vx ,vy = 0 ,0, 0, 0
+    x, y, vx ,vy = x0 ,y0, vx0, vy0
     while x < xmax:
         t += dt
-        x = vx * dt
-        y = vy * dt
+        x += vx * dt
+        y += vy * dt
         vy -= g * dt
+        if y < 0:
+            y = 0
+            vy = -vy * cor
+        
+
